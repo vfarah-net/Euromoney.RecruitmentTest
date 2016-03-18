17f9fde2cd2d204e54c47a3620bf39ee2f2ace76
Vincent Farah <sefrica@live.com>
2016-03-18 12:14:54 +0000

    Story 4 Add a simple console argument for ignoring filtering TODO: Refactor the console client to be tested

--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Castle.Windsor;
 using Domain.Models;
 using Domain.Resolvers;
@@ -7,16 +8,18 @@ namespace ContentConsole
 {
     public static class Program
     {
+        //TODO: Refactor this so tests can be written
         public static void Main(string[] args)
         {
+            bool ignoreFilter = args.Contains("/ignorefiltering", StringComparer.OrdinalIgnoreCase) ||
+                                args.Contains("-ignorefiltering", StringComparer.OrdinalIgnoreCase);
             WindsorContainer windsorContainer = BootstrapConfig.Register();
-            string content =
-                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            string content = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
             IBadWordResolver badWordResolver = windsorContainer.Resolve<IBadWordResolver>();
             InitializeMandatoryBadWords(badWordResolver);
             int badWordsCount = badWordResolver.GetBadWordCount(content);
             Console.WriteLine("Scanned the text:");
-            Console.WriteLine(badWordResolver.Filter(content));
+            Console.WriteLine(ignoreFilter ? content : badWordResolver.Filter(content));
             Console.WriteLine("Total Number of negative words: " + badWordsCount);
             Console.WriteLine("Press ANY key to exit.");
             Console.ReadKey();
